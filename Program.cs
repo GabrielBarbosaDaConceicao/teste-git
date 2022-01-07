80beac3a9d86abc447a9a1a179b1ab963dd76e97
Gabriel Barbosa <gabriel.bc.dev@hotmail.com>
2022-01-07 08:19:49 -0300

    Adicionado uma estrutura de repeticao for para imprimir a tabuada do 2

--- Program.cs
+++ Program.cs
@@ -7,6 +7,12 @@ namespace Test_Para_Git
         {
             Console.WriteLine("Hello, World!!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está\n");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("2" + " X " + i + " = " + i * 2);
+            }
 
             Console.ReadLine();
         }
